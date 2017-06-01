635585797a9f53aac43779e867a71c9bafecdf7d
Andrew Zah <zah@andrewzah.com>
2017-06-01 18:35:00 -0500

    update page name; fix link scrolling

--- js/custom.js
+++ js/custom.js
@@ -44,3 +44,33 @@ $(document).ready(function() {
     $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
   });
 });
+
+(function (jQuery) {
+  jQuery.mark = {
+    jump: function (options) {
+      var defaults = {
+        selector: 'a.scroll-on-page-link'
+      };
+      if (typeof options == 'string') {
+        defaults.selector = options;
+      }
+
+      options = jQuery.extend(defaults, options);
+      return jQuery(options.selector).click(function (e) {
+        var jumpobj = jQuery(this);
+        var target = jumpobj.attr('href');
+        var thespeed = 1000;
+        var offset = jQuery(target).offset().top;
+        jQuery('html,body').animate({
+          scrollTop: offset
+        }, thespeed, 'swing');
+        e.preventDefault();
+      });
+    }
+  };
+})(jQuery);
+
+
+jQuery(function(){  
+  jQuery.mark.jump();
+});
